cdcb73ab64a57bf6d8038b25f8968d7bf5893c42
Joshua Pittman <joshpitzalis@gmail.com>
2020-12-24 17:08:11 +0530

    added derek sivers groups

--- src/features/people/Network.jsx
+++ src/features/people/Network.jsx
@@ -85,6 +85,38 @@ export const InnerNetwork = ({ uid, contactChunks }) => {
             </button>
           </ContactImporter>
         )}
+
+        <div>
+          <div className="sm:hidden">
+            <label htmlFor="tabs" className="sr-only">Select a tab</label>
+            <select id="tabs" name="tabs" className="block w-full focus:ring-green-500 focus:border-green-500 border-gray-300 rounded-md">
+              <option>Every three weeks</option>
+              <option>Every two months</option>
+              <option selected>Every six months</option>
+              <option>Once a year</option>
+            </select>
+          </div>
+          <div className="hidden sm:block">
+            <div className="border-b border-gray-200">
+              <nav className="-mb-px flex" aria-label="Tabs">
+                <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm">
+                Every three weeks
+                </a>
+                <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm">
+                Every two months
+                </a>
+
+                <a href="#" className="border-green-500 text-green-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" aria-current="page">
+                Every six months
+                </a>
+                <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm">
+                Once a year
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+
         <ActiveContactList contacts={allContacts} uid={uid} />
       </div>
     </ErrorBoundary>
